0afab489e620a2acf5f1d6ce57735d0446a10dec
Nitish Nayan <Nitish Nayan@DESKTOP-OECPI9S>
2024-06-07 09:10:45 +0530

    second commit

---
+++ mng_batch/src/main/java/Demo/TestClass.java
@@ -0,0 +1,12 @@
+package Demo;
+
+public class TestClass {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("this program added by lead");
+		// TODO Auto-generated method stub
+
+	}
+
+}
