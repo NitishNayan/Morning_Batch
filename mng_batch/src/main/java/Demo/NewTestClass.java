5e36d5c846463f9e168f321176f42e3e43d3fed5
Nitish Nayan <Nitish Nayan@DESKTOP-OECPI9S>
2024-06-07 09:40:34 +0530

    new class added

--- mng_batch/src/main/java/Demo/NewTestClass.java
+++ mng_batch/src/main/java/Demo/NewTestClass.java
@@ -5,7 +5,7 @@ public class NewTestClass {
 	public static void main(String[] args)
 	{
 
-System.out.println("second program added by lead");
+System.out.println("new  program added by lead");
 	}
 
 }
