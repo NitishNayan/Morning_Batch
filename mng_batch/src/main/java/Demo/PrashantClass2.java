08f893d011f501d83ea9d4d2b6cbfa94fe284744
Prashant <prashant.lakshmanan@gmail.com>
2024-06-11 07:39:12 +0530

    ok

--- mng_batch/src/main/java/Demo/PrashantClass2.java
+++ mng_batch/src/main/java/Demo/PrashantClass2.java
@@ -3,7 +3,9 @@ package Demo;
 public class PrashantClass2 {
 
 	public static void main(String[] args) {
-		System.out.println("Fork Pull testing");
+		System.out.println("Fork Pull testingggg");
+		System.out.println("Fork ");
+		System.out.println("Fork and Pull");
 
 	}
 
