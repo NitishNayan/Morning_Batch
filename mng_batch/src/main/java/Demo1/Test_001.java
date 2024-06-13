1ff9901625165ad47b15bf8e369ed56930e83fb5
Amiya <Amiya@LAPTOP-S0AIS4SN>
2024-06-13 09:15:47 +0530

    build is passed

---
+++ mng_batch/src/main/java/Demo1/Test_001.java
@@ -0,0 +1,14 @@
+package Demo1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test_001 {
+	@Test
+	public void simple()
+	{
+		Reporter.log("build is passed",true);
+	}
+	
+
+}
